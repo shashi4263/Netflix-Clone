3c4572100f06a0d682800ecb08dd5067c1ac2d8d
shashikiller <rshashi133@gmail.com>
2024-07-05 18:53:21 +0530

    Movie section

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,6 +1,12 @@
+import MovieVideo from "../components/MovieVideo"
 import Navbar from "../components/Navbar"
-export default function HomePage(){
-    return(
-        <h1>helloo from home</h1>
+import RecentlyAdded from "../components/RecentlyAdded"
+export default function HomePage() {
+    return (
+        <div className="p-5 lg:p-0">
+            <MovieVideo />
+            <h1 className="text-3xl font-bold ">Recently Added</h1>
+            <RecentlyAdded />
+        </div>
     )
 }
