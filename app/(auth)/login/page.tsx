55d24618f2ee89827f342c58e9de1449453053ca
shashikiller <rshashi133@gmail.com>
2024-07-06 00:04:47 +0530

    updated login and sign-up

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,39 +1,50 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
-import { getServerSession } from "next-auth";
 import GithubSignInButton from "@/app/components/GithubSignInButton";
 import GoogleSignInButton from "@/app/components/GoogleSignInButton";
+import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/utils/auth";
 import { redirect } from "next/navigation";
 
-
-export default async function Login(){
+export default async function Login() {
     const session = await getServerSession(authOptions);
 
-    if(session){
+    if (session) {
         return redirect("/home");
     }
-    return(
-        <div className="mt-24 rounded bg-black/70 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
+    return (
+        <div className="mt-24  rounded bg-black/80 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
             <form method="post" action="/api/auth/signin">
-                <h1 className="text-3xl font-semibold text-white ">Login</h1>
+                <h1 className="text-3xl font-semibold text-white">Login</h1>
                 <div className="space-y-4 mt-5">
-                    <Input type="email" name="email" placeholder="Email" 
-                    className="bg-[#333] placeholder:text-xs placeholder:text-gray-400 w-full inline-block"/>
-                    <Button type="submit" variant={"destructive"} className="w-full bg-[#E50914]">Login</Button>
+                    <Input
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        className="bg-[#333] placeholder:text-xs placeholder:text-gray-400 w-full inline-block"
+                    />
+                    <Button
+                        type="submit"
+                        variant="destructive"
+                        className="w-full bg-[#e50914]"
+                    >
+                        Log in
+                    </Button>
                 </div>
             </form>
+
             <div className="text-gray-500 text-sm mt-2">
-                New to Netflix? <Link className="text-white hover:underline" href="/sign-up">
-                Sign Up</Link>
+                New to Neflix?{" "}
+                <Link className="text-white hover:underline" href="/sign-up">
+                    Sign up now
+                </Link>
             </div>
 
             <div className="flex w-full justify-center items-center gap-x-3 mt-6">
                 <GithubSignInButton />
                 <GoogleSignInButton />
-                        
             </div>
         </div>
     );
-};
+}
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,42 +1,49 @@
-import Image from "next/image";
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Link from "next/link";
-import { GithubIcon } from "lucide-react";
-import GoogleIcon from '../../../public/google.svg'
+import GithubSignInButton from "@/app/components/GithubSignInButton";
+import GoogleSignInButton from "@/app/components/GoogleSignInButton";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/utils/auth";
 import { redirect } from "next/navigation";
 
-export default async function SignUp(){
+export default async function SignUp() {
     const session = await getServerSession(authOptions);
-
-    if(session){
+    if (session) {
         return redirect("/home");
     }
-    return(
-        <div className="mt-24 rounded bg-black/70 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
+    return (
+        <div className="mt-24  rounded bg-black/80 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
             <form method="post" action="/api/auth/signin">
-                <h1 className="text-3xl font-semibold text-white ">Sign Up</h1>
+                <h1 className="text-3xl font-semibold text-white">Sign Up</h1>
                 <div className="space-y-4 mt-5">
-                    <Input type="email" name="email" placeholder="Email" 
-                    className="bg-[#333] placeholder:text-xs placeholder:text-gray-400 w-full inline-block"/>
-                    <Button type="submit" variant={"destructive"} className="w-full bg-[#E50914]">Sign Up</Button>
+                    <Input
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        className="bg-[#333] placeholder:text-xs placeholder:text-gray-400 w-full inline-block"
+                    />
+                    <Button
+                        type="submit"
+                        variant="destructive"
+                        className="w-full bg-[#e50914]"
+                    >
+                        Sign Up
+                    </Button>
                 </div>
             </form>
+
             <div className="text-gray-500 text-sm mt-2">
-                Already have a account? <Link className="text-white hover:underline" href="/login">
-                Login</Link>
+                Already Have a account?{" "}
+                <Link className="text-white hover:underline" href="/login">
+                    Login now!
+                </Link>
             </div>
 
             <div className="flex w-full justify-center items-center gap-x-3 mt-6">
-                <Button variant={"outline"} size={"icon"}>
-                    <GithubIcon className="w-4 h-4" />
-                </Button>
-                <Button variant={"outline"} size={"icon"}>
-                    <Image src={GoogleIcon} alt="googleIcon" className="w-6 h-6"/>
-                </Button>        
+                <GithubSignInButton />
+                <GoogleSignInButton />
             </div>
         </div>
     );
-};
+}
